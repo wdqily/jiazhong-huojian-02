554816f24a0d227ca0ff69c51d50789781dfa04c
walker <3779075559@qq.com>
2026-03-22 16:28:48 +0800

    boot-ioc-mybatis

---
+++ jiazhong-spring-boot/boot-ioc-mybatis/src/main/java/com/jiazhong/huojian/spring/boot/IoC/mybatis/BootIoCMybatisApplication.java
@@ -0,0 +1,11 @@
+package com.jiazhong.huojian.spring.boot.IoC.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication//启动类
+public class BootIoCMybatisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootIoCMybatisApplication.class, args);
+    }
+}
