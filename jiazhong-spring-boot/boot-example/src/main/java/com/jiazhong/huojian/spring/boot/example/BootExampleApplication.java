6144835f0ea768564d45b924391000d27296968b
walker <3779075559@qq.com>
2026-03-28 11:46:56 +0800

    spring-aop

---
+++ jiazhong-spring-boot/boot-example/src/main/java/com/jiazhong/huojian/spring/boot/example/BootExampleApplication.java
@@ -0,0 +1,12 @@
+package com.jiazhong.huojian.spring.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//这个项目的启动类
+@SpringBootApplication
+public class BootExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootExampleApplication.class, args);
+    }
+}
